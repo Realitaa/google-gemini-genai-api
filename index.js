1fe6fb12e9fece25d983a4fcd680da0e551e13cb
Realitaa <rezamuliaputra927@gmail.com>
2025-07-16 20:28:16 +0700

    feat: add endpoint /generate-text for generate text

--- index.js
+++ index.js
@@ -12,16 +12,33 @@ const port = process.env.PORT;
 const ai = new GoogleGenAI({});
 
 async function genAI(prompt) {
-    const response = await ai.models.generateContent({
-      model: process.env.GEMINI_MODEL,
-      contents: prompt,
-    });
-    return response.text;
-  }
+  const response = await ai.models.generateContent({
+    model: process.env.GEMINI_MODEL,
+    contents: prompt,
+  });
+  return response.text;
+}
 
 app.get('/', (req, res) => {
   res.send('Hello Gemini!')
 })
+
+app.post('/generate-text', async (req, res) => {
+  const { prompt } = req.body;
+  
+  try {
+    const result = await genAI(prompt);
+    res.json({
+        status: 'success',
+        result
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: 'success',
+      message: error.message,
+    });
+  }
+});
 
 app.listen(port, () => {
   console.log(`Gemini API server is running at http://localhost:${port}`);
