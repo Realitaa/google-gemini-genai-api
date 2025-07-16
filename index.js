424c02e115d4c08a99b8a0e0f39f635c49fcbde0
Realitaa <rezamuliaputra927@gmail.com>
2025-07-16 21:09:53 +0700

    feat: add endpoint /generate-from-image

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
 import express from 'express';
 import 'dotenv/config';
-// import multer from 'multer';
-// import path from 'node:path';
+import multer from 'multer';
+import path from 'path';
+import fs from 'node:fs';
 import { GoogleGenAI } from "@google/genai";
 
+// Setup storage destination & filename
+const uploadsDir = path.join(process.cwd(), 'uploads');
+
+if (!fs.existsSync(uploadsDir)) {
+  fs.mkdirSync(uploadsDir);
+}
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'uploads/'); // simpan ke folder uploads
+  },
+  filename: (req, file, cb) => {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+    cb(null, uniqueSuffix + '-' + file.originalname);
+  },
+});
+
+const upload = multer({ storage });
+
+// Express configuration
 const app = express();
 app.use(express.json());
 const port = process.env.PORT;
@@ -39,6 +60,73 @@ app.post('/generate-text', async (req, res) => {
     });
   }
 });
+
+app.post('/generate-from-image', upload.single('image'), async (req, res) => {
+  try {
+    const prompt = req.body.prompt || 'Describe this image in 3 sentences.';
+
+    // Pastikan file terupload
+    if (!req.file) {
+      return res.status(400).json({
+        status: 'error',
+        message: 'No image file uploaded.',
+      });
+    }
+
+    const image = req.file.path;
+
+    // Baca file sebagai base64
+    const base64ImageFile = fs.readFileSync(image, {
+      encoding: 'base64',
+    });
+
+    if (!base64ImageFile) {
+      return res.status(500).json({
+        status: 'error',
+        message: 'Failed to read the uploaded image file.',
+      });
+    }
+
+    const contents = [
+      {
+        inlineData: {
+          mimeType: req.file.mimetype,
+          data: base64ImageFile,
+        },
+      },
+      { text: prompt },
+    ];
+
+    const result = await genAI(contents);
+
+    res.json({
+      status: 'success',
+      result,
+    });
+
+    // Hapus file setelah response berhasil dikirim
+    fs.unlink(imagePath, (err) => {
+      if (err) {
+        console.error('Gagal menghapus file:', err);
+      } else {
+        console.log('File berhasil dihapus:', imagePath);
+      }
+    });
+  } catch (err) {
+    console.error('Error in /generate-from-image:', err);
+    res.status(500).json({
+      status: 'error',
+      message: err.message || 'Internal Server Error',
+    });
+
+    // Hapus file jika error jika file berhasil di upload
+    if (req.file?.path) {
+      fs.unlink(req.file.path, (err) => {
+        if (err) console.error('Gagal menghapus file saat error:', err);
+      });
+    }
+  }
+});  
 
 app.listen(port, () => {
   console.log(`Gemini API server is running at http://localhost:${port}`);
